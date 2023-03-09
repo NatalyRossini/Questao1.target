cfd328fc53e779c862fed7c5fb0d0af02f7c3b24
Nataly Rossini <natalyrossinii@gmail.com>
2023-03-08 21:00:13 -0300

    questao1

---
+++ codigo1.py
@@ -0,0 +1,8 @@
+#Questao 1 - Processo seletivo Target.
+#Optei pela linguagem Python.
+
+indice, soma, k = 13, 0, 0  #declaração das variáveis.
+while (k < indice):         #estrutura de repetição.
+    k +=1
+    soma = soma + k
+print (soma)                #saída do resultado.
